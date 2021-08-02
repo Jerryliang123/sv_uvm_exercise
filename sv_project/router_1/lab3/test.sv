d62b2f186bd472b9c839024c8d05e7a99f61c729
jerry <jerryliang9609@gmail.com>
2021-08-02 16:07:41 +0800

    cinnut

--- sv_project/router_1/lab3/test.sv
+++ sv_project/router_1/lab3/test.sv
@@ -1,5 +1,7 @@
 program automatic test(router_io.TB rtr_io);
 
+    logic [7:0] pkt2cmp_payload;
+
     bit [3:0] sa;
     bit [3:0] da;
     logic [7:0] payload[$]; // payload queue every payload is a byte
@@ -10,10 +12,86 @@ program automatic test(router_io.TB rtr_io);
         reset();
         repeat (run_for_n_packets) begin
             gen();
-            send();
+            fork
+                send();
+                recv();
+            join
+            check();
         end
     end
+// for recv
+    task recv();
+        get_payload();
+    endtask
 
+    task get_payload();
+        pkt2cmp_payload.delete();
+        fork
+            begin: wd_timer_fork
+                fork:frameo_wd_timer // if frameo_n negedge arrived then jump out fork
+                    @(negedge rtr_io.cb.frame_n[da]);
+                    begin
+                        repeat(1000) @(rtr_io.cb);
+                        $display("\n%m\n[ERROR]%t Frame signal timed out!\n", $realtime);
+                        $finish;
+                    end
+                join_any:frameo_wd_timer
+                disable fork;
+            end:wd_timer_fork
+        join
+
+        forever begin
+            logic [7:0] datum;
+            for (int i=0; i<8;) begin
+                if(!rtr_io.cb.valido_n[da])
+                    datum[i++] = rtr_io.cb.dout[da];
+                if (rtr_io.cb.frameo_n[da]) begin
+                    if(i==8) begin
+                        pkt2cmp_payload.push_back(datum);
+                        return;
+                    end
+                    else begin
+                        $display("\n%m\n[ERROR]%t Packet payload not byte aligned!\n", $realtime);
+                        $finish;
+                    end
+                end
+                @(rtr_io.cb);               
+            end
+            pkt2cmp_payload.push_back(datum);
+        end
+
+    endtask: get_payload
+
+// for checker and compare
+    function bit compare(ref string message);
+        if (payload.size() != pkt2cmp_payload.size()) begin
+            message = "payload size mismatch: \n";
+            message = {message, $sformatf("payload.size() = %0d, pkt2cmp_payload.size() = %0d\n",payload.size(),pkt2cmp_payload.size())};
+            return (0);
+        end
+
+        if (payload == pkt2cmp_payload);
+        else begin
+            message = "Payload Content Mismatch:\n";
+            message = {message, $sformatf("Packet Sent: %p\nPkt Received: %p", payload, pkt2cmp_payload)};
+            return (0);
+        end
+        message = "successfully Compared";
+        return(1); 
+    endfunction
+
+    task check();
+        string message;
+        static int pkts_checked = 0;
+        if(!compare(message)) begin
+            $display("\n%m\n[ERROR]%t Packet #%0d %s\n", $realtime, pkts_checked,message)
+            $finish;
+        end
+        $display("[NOTE]%t Packet #%0d %s", $realtime, pkts_checked++, message);
+    endtask    
+
+
+//reset
     task reset();
         rtr_io.reset_n = 1'b0;
         rtr_io.cb.frame_n <= '1;
@@ -22,6 +100,7 @@ program automatic test(router_io.TB rtr_io);
         repeat(15) @(rtr_io.cb);
     endtask 
 
+//driver
 // generate stimulus
     task gen();
         sa = 3;
